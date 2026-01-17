b01bee4d3877f2871d8bf002347a0f2dd4905c49
amlord78 <88088117+amlord78@users.noreply.github.com>
2026-01-17 15:39:20 +0530

    Update AppGraph.kt

--- app/src/main/java/com/farchase/indicfriendchat/data/AppGraph.kt
+++ app/src/main/java/com/farchase/indicfriendchat/data/AppGraph.kt
@@ -1,16 +1,10 @@
 package com.farchase.indicfriendchat.data
 
 import android.content.Context
-import com.farchase.indicfriendchat.data.net.Network
 
 class AppGraph(context: Context) {
     private val ctx = context.applicationContext
 
     val profileStore: ProfileStore = ProfileStore(ctx)
-
-    // ✅ ChatRepository(api, profileStore)
-    val repo: ChatRepository = ChatRepository(
-        api = Network.api,
-        profileStore = profileStore
-    )
+    val repo: ChatRepository = ChatRepository(ctx)
 }
